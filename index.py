2995a99121ce27ebdfa649d0a03171531c3fde78
gnobile <giorgio.nobile@gmail.com>
2020-04-22 13:13:02 +0200

    Update index.py
    modified the way Lambda access CloudFormation Stack Name

--- index.py
+++ index.py
@@ -1,20 +1,3 @@
-'''
-Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this
-software and associated documentation files (the "Software"), to deal in the Software
-without restriction, including without limitation the rights to use, copy, modify,
-merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
-permit persons to whom the Software is furnished to do so.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-'''
-
 import boto3
 import json
 import os
@@ -127,7 +110,13 @@ def response_function(status_code, response_body):
 def autoscaling_policy_arn(context):
 	print (context.function_name)
 	function_name = context.function_name
-	stack_name = context.function_name.split('LambdaScaler')[0][:-1]
+	print(context.invoked_function_arn)
+	tags = client_lambda.list_tags(
+    		Resource=context.invoked_function_arn
+			)
+
+	print(tags)
+	stack_name = tags['Tags']['aws:cloudformation:stack-name']
 	print (stack_name)
 	
 	response = client_cloudformation.describe_stack_resources(
